c44af497046c32606df6c5e2bda22297f3892d1f
Hussain-hamim <mohammadhussainafghan83@gmail.com>
2024-04-18 19:46:24 +0430

    contactme section debugging...

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -59,7 +59,7 @@ const ContactMeSection = () => {
       py={16}
       spacing={8}
     >
-      <VStack w="1024px" p={32} alignItems="flex-start">
+      <VStack alignItems="flex-start">
         <Heading as="h1" id="contactme-section">
           Contact me
         </Heading>
