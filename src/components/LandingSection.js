46667195a296ec73254256937aab6df34cbd4808
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-25 20:14:02 +0430

    refactor

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -5,6 +5,7 @@ import FullScreenSection from "./FullScreenSection";
 import hamim from "../asset/hhamim.jpg";
 import "./styles.css";
 import "../App.css";
+// import ParticleBackground from "./ParticleBackground";
 
 // const greeting = "Hello, I am <span class="hussain">Hussain!</span>";
 const bio1 = "A frontend developer";
@@ -29,35 +30,38 @@ const LandingSection = ({ darkMode }) => {
   // setTimeout(() => typeWriter(aboutSection, originalText), 1500);
 
   return (
-    <FullScreenSection
-      justifyContent="center"
-      alignItems="center"
-      backgroundColor={darkMode ? "#CBD5E0" : "#212529"}
-      isDarkBackground={darkMode}
-      className="newBg"
-    >
-      <VStack pt={32} mb={32} spacing={16}>
-        <VStack spacing={4} alignItems="center">
-          <Avatar className="prof" src={hamim} size="2xl" name="HHamim" />
-          <Heading as="h5" size="sm" noOfLines={1}>
-            <div class="typewriter-container">
-              <h1 class="typewriter typewriter-container">
-                Hello, I am
-                <span className="hussain">Hussain</span>!
-              </h1>
-            </div>
-          </Heading>
+    <div>
+      {/* <ParticleBackground></ParticleBackground> */}
+      <FullScreenSection
+        justifyContent="center"
+        alignItems="center"
+        backgroundColor={darkMode ? "#CBD5E0" : "#212529"}
+        isDarkBackground={darkMode}
+        className="newBg"
+      >
+        <VStack pt={32} mb={32} spacing={16}>
+          <VStack spacing={4} alignItems="center">
+            <Avatar className="prof" src={hamim} size="2xl" name="HHamim" />
+            <Heading as="h5" size="sm" noOfLines={1}>
+              <div class="typewriter-container">
+                <h1 class="typewriter typewriter-container">
+                  Hello, I am
+                  <span className="hussain">Hussain</span>!
+                </h1>
+              </div>
+            </Heading>
+          </VStack>
+          <VStack spacing={6}>
+            <Heading as="h3" size="2xl" noOfLines={1}>
+              {bio1}
+            </Heading>
+            <Heading as="h3" size="2xl" noOfLines={1}>
+              {bio2}
+            </Heading>
+          </VStack>
         </VStack>
-        <VStack spacing={6}>
-          <Heading as="h3" size="2xl" noOfLines={1}>
-            {bio1}
-          </Heading>
-          <Heading as="h3" size="2xl" noOfLines={1}>
-            {bio2}
-          </Heading>
-        </VStack>
-      </VStack>
-    </FullScreenSection>
+      </FullScreenSection>
+    </div>
   );
 };
 
