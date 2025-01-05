834f4a0b0bd0bd4e99e2f7dc3e3cd5a1745e1156
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-01-05 20:05:12 +0430

    skill icons

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 
-import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import { Avatar, Heading, VStack, HStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 import hamim from "../asset/hhamim3.jpg";
 import "./styles.css";
@@ -12,11 +12,20 @@ import {
   faGit,
   faGithub,
   faHtml5,
+  faIntercom,
   faJs,
   faNodeJs,
+  faPerbyte,
   faReact,
 } from "@fortawesome/free-brands-svg-icons";
-import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCloud,
+  faDatabase,
+  faLaptopCode,
+  faMobile,
+  faServer,
+  faTools,
+} from "@fortawesome/free-solid-svg-icons";
 import { keyframes } from "framer-motion";
 // import ParticleBackground from "./ParticleBackground";
 
@@ -82,26 +91,28 @@ const LandingSection = ({ darkMode }) => {
             through innovative and pixel perfect design.
           </div>
 
-          <div style={{ textAlign: "center", fontFamily: "fantasy" }}>
-            <Heading as="h4" size="1x">
+          <div style={{ textAlign: "center" }}>
+            <Heading as="h4" size="1x" fontFamily="serif">
               Skills
             </Heading>
 
-            <div
-              style={{
-                display: "flex",
-                gap: "20px",
-              }}
-              // className="marquee"
-            >
+            <div style={{ display: "flex", gap: 30, paddingBottom: 20 }}>
+              <FontAwesomeIcon icon={faLaptopCode} size="2x" color="green" />
               <FontAwesomeIcon icon={faJs} size="2x" color="tomato" />
               <FontAwesomeIcon icon={faReact} size="2x" color="lightblue" />
-              <FontAwesomeIcon icon={faNodeJs} size="2x" color="tomato" />
+              <FontAwesomeIcon icon={faNodeJs} size="2x" color="green" />
+              <FontAwesomeIcon icon={faServer} size="2x" color="lightblue" />
+              <FontAwesomeIcon icon={faTools} size="2x" color="gray" />
               <FontAwesomeIcon icon={faGithub} size="2x" color="gray" />
+            </div>
+            <div style={{ display: "flex", gap: 30 }}>
               <FontAwesomeIcon icon={faHtml5} size="2x" color="tomato" />
               <FontAwesomeIcon icon={faCss3} size="2x" color="tomato" />
-              <FontAwesomeIcon icon={faDatabase} size="2x" color="tomato" />
+              <FontAwesomeIcon icon={faDatabase} size="2x" color="lightblue" />
               <FontAwesomeIcon icon={faGit} size="2x" color="tomato" />
+              <FontAwesomeIcon icon={faMobile} size="2x" color="gray" />
+              <FontAwesomeIcon icon={faFigma} size="2x" color="orchid" />
+              <FontAwesomeIcon icon={faCloud} size="2x" color="orchid" />
             </div>
           </div>
         </VStack>
