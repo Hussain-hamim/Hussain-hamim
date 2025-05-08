719a674c2c76fcc4458d11dada9aa16106333e32
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-05-08 20:14:48 +0430

    add Vercel Analytics for improved performance tracking and update dependencies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.css";
-import "./App.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import App from './App';
+import { Analytics } from '@vercel/analytics/react';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
 
