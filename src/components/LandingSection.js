f2ea6026885b30f048d73af0d9c9e05b53afc022
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-09-17 23:04:51 +0430

    Change image source to hussain-gray.jpg

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { keyframes, css } from '@emotion/react';
 import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
-import hamim from '../asset/hussain.jpg';
+import hamim from '../asset/hussain-gray.jpg';
 import FullScreenSection from './FullScreenSection';
 
 // Water ripple animation for bg
